c6b4ac48cc73d8481035de5a48407b6b863c8178
Theo Denovan <theo@t6d.com>
2014-02-10 21:26:07 +1100

    jquery ui dialog

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.ui.autocomplete
 //= require jquery.ui.datepicker
+//= require jquery.ui.dialog
 //= require turbolinks
 //= require rails.validations.js
 //= require layout/jquery.dropotron
