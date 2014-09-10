f730ab740d31ec69c5d1f48e3997ceffc9dfb2f5
Theo Denovan <theo@t6d.com>
2014-09-10 21:24:15 +1000

    updates

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -1,8 +1,7 @@
 var opthoApp = angular.module('opthoApp', []);
 
-console.log('hi');
 opthoApp.controller('CalendarListCtrl', ['$scope', '$resource', function($scope, $resource) {
     var Appointments = $resource('/appointments');
     console.log('hi');
-    $scope.appointments = Appointment.query();
+    $scope.appointments = Appointments.query();
 }])
