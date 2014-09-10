8b2b96fd77be4e5e64ca1f32d3cac0f7cfe09fc2
Theo Denovan <theo@t6d.com>
2014-09-10 21:38:59 +1000

    added timestamp

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -1,5 +1,7 @@
 opthoApp.controller('CalendarListCtrl', ['$scope', '$resource', function($scope, $resource) {
-    var Appointments = $resource('/appointments');
-    console.log('hi');
-    $scope.appointments = Appointments.query();
+    var Appointments = $resource('/appointments/findByDate');
+    today = new Date();
+    yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+    $scope.appointments = Appointments.query({start: today.getTime(), end: yesterday.getTime()});
 }])
