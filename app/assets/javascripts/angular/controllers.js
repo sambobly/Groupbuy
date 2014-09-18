b4a59796515efad586c86cb93729bdc53fbc76cd
Thomas Conrad <thomas.conrad@uqconnect.edu.au>
2014-09-18 22:08:44 +1000

    Adding image widget. Where to put json data?

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -5,3 +5,12 @@ opthoApp.controller('CalendarListCtrl', ['$scope', '$resource', function($scope,
     yesterday.setDate(today.getDate() - 1);
     $scope.appointments = Appointments.query({start: today.getTime(), end: yesterday.getTime()});
 }])
+
+opthoApp.controller('ImageCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('app/sampleimages.json').success(function(data) {
+            $scope.images = data;
+        });
+
+        $scope.orderProp = 'age';
+    }]);
