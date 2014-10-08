2a0b2ad22f6fc645ff2759a25c00c5556ac39478
Thomas Conrad <thomas.conrad@uqconnect.edu.au>
2014-10-08 14:10:09 +1000

    Reset changes to fix error

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -7,18 +7,14 @@ opthoControllers.controller('CalendarListCtrl', ['$scope', '$resource', function
     yesterday.setDate(today.getDate() - 1);
     $scope.appointments = Appointments.query({start: today.getTime(), end: yesterday.getTime()});
 }]);
-
-
 opthoControllers.controller('ImageCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
-            $http.get('/assets/sampleimages.json').success(function(data) {
-                $scope.images = data.images;
-                $scope.mainImageUrl = data.images[0];
-            });
-}]);
-
-opthoControllers.controller('PtntInfoCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
-    $http.get('/assets/samplepatients.json').success(function(data) {
-            $scope.patients = data.patients;
+    $http.get('/assets/sampleimages.json').success(function(data) {
+        $scope.images = data.images;
+        $scope.mainImageUrl = data.images[0];
     });
 }]);
-
+opthoControllers.controller('PtntInfoCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    $http.get('/assets/samplepatients.json').success(function(data) {
+        $scope.patients = data.patients;
+    });
+}]);
