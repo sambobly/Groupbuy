9a9fe5ede209222a9c892604b38f4c107ee61b7c
Thomas Conrad <thomas.conrad@uqconnect.edu.au>
2014-11-06 09:41:27 +1000

    Roughly halfway through implementing email functionality

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -25,7 +25,15 @@ opthoControllers.controller('PathologyController', ['$scope', '$routeParams', '$
     $scope.queryBy = '$'
     $http.get('/assets/samplepathology.json').success(function(data) {
         $scope.pathology = data.pathology;
+        $scope.isPopupVisible = false;
+        $scope.showPopup = function(pathology) {
+            $scope.isPopupVisible = true;
+        };
+        $scope.closePopup = function(){
+            $scope.isPopupVisible = false
+        };
     });
+
 }]);
 opthoControllers.controller('ConsultController', ['$scope', function($scope) {
     $scope.master = {};
@@ -43,16 +51,36 @@ opthoControllers.controller('ConsultController', ['$scope', function($scope) {
 }])
 opthoControllers.controller('MedicationController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
     $http.get('/assets/samplemedications.json').success(function(data) {
-        $scope.medications = data.medications
+        $scope.medications = data.medications;
         $scope.pastmedications = [
             {name: 'Pantoprazole'},
             {name: 'Pregabalin'}];
         $scope.mypastmedications = $scope.pastmedications[2];
+        $scope.newmedications = data.newmedication
+        $scope.isPopupVisible = false;
+        $scope.showPopup = function(newmedication) {
+            $scope.isPopupVisible = true;
+        };
+        $scope.closePopup = function(){
+            $scope.isPopupVisible = false
+        };
+        $scope.saveMedication = function() {
+            $scope.isPopupVisible = false
+            $scope.newmedications.push($scope.medications);
+        };
     });
 }]);
 opthoControllers.controller('PastAppointmentsController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
     $http.get('/assets/samplepastappointments.json').success(function(data) {
         $scope.pastappointments = data.pastappointments
+        $scope.isPopupVisible = false;
+        $scope.showPopup = function(pastappointment) {
+            $scope.isPopupVisible = true;
+            $scope.selectedpastappointment = pastappointment;
+        };
+        $scope.closePopup = function() {
+            $scope.isPopupVisible = false
+        }
     });
 }]);
 opthoControllers.controller('EmailController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
@@ -88,5 +116,57 @@ opthoControllers.controller('EmailController', ['$scope', '$routeParams', '$http
             $scope.isPopupVisible = false;
         };
         $scope.activeTab = "inbox";
+        $scope.forward = function() {
+            $scope.isPopupVisible = false;
+            $scope.composeEmail = {};
+            angular.copy($scope.selectedEmail, $scope.composeEmail);
+
+            $scope.composeEmail.body =
+                "\n-------------------------------\n"
+                    + "from: " + $scope.composeEmail.from + "\n"
+                    + "sent: " + $scope.composeEmail.date + "\n"
+                    + "to: " + $scope.composeEmail.to + "\n"
+                    + "subject: " + $scope.composeEmail.subject + "\n"
+                    + $scope.composeEmail.body;
+
+            $scope.composeEmail.subject = "FW: " + $scope.composeEmail.subject;
+            $scope.composeEmail.to = "";
+            $scope.composeEmail.from = "me";
+            $scope.isComposePopupVisible = true;
+        };
+        $scope.reply = function() {
+            // hide the view details popup
+            $scope.isPopupVisible = false;
+
+            // create an empty composeEmail object the compose
+            // email popup is bound to
+            $scope.composeEmail = {};
+
+            // copy the data from selectedEmail into composeEmail
+            angular.copy($scope.selectedEmail, $scope.composeEmail);
+
+            // edit the body to prefix it with a line and the
+            // original email information
+            $scope.composeEmail.body =
+                "\n-------------------------------\n"
+                    + "from: " + $scope.composeEmail.from + "\n"
+                    + "sent: " + $scope.composeEmail.date + "\n"
+                    + "to: " + $scope.composeEmail.to + "\n"
+                    + "subject: " + $scope.composeEmail.subject + "\n"
+                    + $scope.composeEmail.body;
+
+            // prefix the subject with “RE:”
+            $scope.composeEmail.subject = "RE: " + $scope.composeEmail.subject;
+
+            // the email is going to the person who sent it
+            // to us so populate the `to` with `from`
+            $scope.composeEmail.to = $scope.composeEmail.from;
+
+            // it’s coming from us
+            $scope.composeEmail.from = "me";
+
+            // show the compose email popup
+            $scope.isComposePopupVisible = true;
+        };
     });
 }]);
