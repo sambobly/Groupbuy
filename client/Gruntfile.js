267fcd8a96839c429a4eb2a6c1647ce93df5e21e
Thomas Conrad <thomas.conrad@uqconnect.edu.au>
2015-04-01 16:32:22 +1000

    Trying to deploy to heroku to allow fb devl, cchanged database to production progsql

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -9,6 +9,10 @@
 
 module.exports = function (grunt) {
 
+// Project configuration.
+    var pkg = require('./package.json');
+
+
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
 
@@ -429,7 +433,37 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.coffee',
         singleRun: true
       }
-    }
+    },
+      // Various Grunt tasks...
+
+      buildcontrol: {
+          options: {
+              dir: 'dist',
+              commit: true,
+              push: true,
+              message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+          },
+          pages: {
+              options: {
+                  remote: 'git@github.com:example_user/example_webapp.git',
+                  branch: 'gh-pages'
+              }
+          },
+          heroku: {
+              options: {
+                  remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+                  branch: 'master',
+                  tag: pkg.version
+              }
+          },
+          local: {
+              options: {
+                  remote: '../',
+                  branch: 'build'
+              }
+          }
+      }
+
   });
 
 
